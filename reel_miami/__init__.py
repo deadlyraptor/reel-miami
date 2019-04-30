880404123def180e47672c25d18d921695d41360
Javier Chavez <jachavez6@gmail.com>
2019-04-29 23:25:10 -0400

    Introduce database for venues, films, and showtimes
    In the event that a ticketing URL is not available, the database is the
    first step in manually entering showtime data. A future commit will
    introduce the option of entering this data via a specially formatted
    CSV file.

--- reel_miami/__init__.py
+++ reel_miami/__init__.py
@@ -1,12 +1,22 @@
 # __init__.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    from reel_miami import models
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from reel_miami.main.routes import main
     from reel_miami.utils.filters import filters
---
+++ reel_miami/models.py
@@ -0,0 +1,63 @@
+# models.py
+
+from reel_miami import db
+
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address1 = db.Column(db.String, nullable=False)
+    address2 = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
+    postal_code = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    venue_photo = db.Column(db.String(20), nullable=False,
+                            default='default.jpg')
+
+    films = db.relationship('Film', backref='venue', lazy=True)
+
+    def __repr__(self):
+        return f'<Venue(name={self.name})>'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Film(db.Model):
+    __tablename__ = 'films'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    running_time = db.Column(db.String, nullable=False)
+    director = db.Column(db.String, nullable=False)
+    year = db.Column(db.String, nullable=False)
+
+    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'))
+
+    showtimes = db.relationship('Showtime', backref='film', lazy=True)
+
+    def __repr__(self):
+        return f'<Film(name={self.name})>'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Showtime(db.Model):
+    __tablename__ = 'showtimes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    time = db.Column(db.String, nullable=False)
+    ticketing_link = db.Column(db.String, nullable=False)
+
+    film_id = db.Column(db.Integer, db.ForeignKey('films.id'))
+
+    def __repr__(self):
+        return f'<Showtime(date={self.date}, time={self.time})>'
+
+    def __str__(self):
+        return f'{self.name}'
