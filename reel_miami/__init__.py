b8530d4e18a1a66525172fdd1feb1d488c56c0df
Javier Chavez <jachavez6@gmail.com>
2019-05-26 16:39:22 -0400

    Implement flask-admin
    An admin portal will allow simple CRUD operations without having to mess 
    with the database itself.

--- reel_miami/__init__.py
+++ reel_miami/__init__.py
@@ -1,12 +1,15 @@
 # __init__.py
 
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+admin = Admin()
 
 
 def create_app(config_class=Config):
@@ -17,6 +20,9 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    admin.init_app(app)
+
+    admin.add_view(ModelView(models.Venue, db.session))
 
     from reel_miami.main.routes import main
     from reel_miami.utils.filters import filters
